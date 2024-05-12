2aa483a15e96369a5bfd854506defe80d8c6737a
MrWonkles <mrwonkles@gmail.com>
2024-05-12 12:14:38 -0400

    Initial version of the Rock, Paper, Scissors game.

---
+++ RPS.py
@@ -0,0 +1,102 @@
+import random as ran
+from time import sleep
+
+def playerStartCheck():
+
+    playGame = input("Type Y or N: ")
+
+    if playGame == "Y" or playGame == "y":
+        print("\n")
+        sleep(1)
+        startMenu()
+    elif playGame == "N" or playGame == "n":
+        print("Goodbye!\n")
+        sleep(1)
+        exit()
+    else:
+        print("That's not a correct choice.\n")
+        playerStartCheck()
+
+def startMenu():
+
+    print("------------------------------------------")
+    print("  Beat 3 rounds and you win! Good luck!")
+    print("------------------------------------------\n")
+    playerChooses()
+
+def playerChooses():
+    print("1 -- Rock\n2 -- Paper\n3 -- Scissors\n")
+
+    global playerChoice
+    playerChoice = input("Choose... ")
+
+    if playerChoice == '1' or playerChoice == '2' or playerChoice == '3':
+        if playerChoice == '1': playerChoice = rpsChoices[0]
+        if playerChoice == '2': playerChoice = rpsChoices[1]
+        if playerChoice == '3': playerChoice = rpsChoices[2]
+        print(f"You chose {playerChoice}.")
+        compChooses()
+    else:
+        playerChooses()
+
+def compChooses():
+    global compChoice
+    compChoice = ran.choice(rpsChoices)
+    sleep(1)
+    print(f"The computer chose {compChoice}.\n")
+    sleep(1)
+    roundEval()
+
+def roundEval():
+    global compScore
+    global playerScore
+
+    if playerChoice == rpsChoices[0]:
+        if compChoice == rpsChoices[1]:
+            print(f"{compChoice} beats {playerChoice}. You LOSE!")
+            compScore = compScore + 1
+        elif compChoice == rpsChoices[2]:
+            print(f"{playerChoice} beats {compChoice}. You WIN!")
+            playerScore = playerScore + 1
+        else: print("TIE!")  
+
+    elif playerChoice == rpsChoices[1]:
+        if compChoice == rpsChoices[2]:
+            print(f"{compChoice} beats {playerChoice}. You LOSE!")
+            compScore = compScore + 1
+        elif compChoice == rpsChoices[0]:
+            print(f"{playerChoice} beats {compChoice}. You WIN!")
+            playerScore = playerScore + 1
+        else: print("TIE!") 
+
+    elif playerChoice == rpsChoices[2]:
+        if compChoice == rpsChoices[0]:
+            print(f"{compChoice} beats {playerChoice}. You LOSE...")
+            compScore = compScore + 1
+        elif compChoice == rpsChoices[1]:
+            print(f"{playerChoice} beats {compChoice}. You WIN!")
+            playerScore = playerScore + 1
+        else: print("TIE!")
+
+    sleep(1)
+    print(f"\nYour score: {playerScore}\tComputer score: {compScore}\n")
+    sleep(1)
+
+    if playerScore >= 3:
+        print("You win the game!\n")
+    elif compScore >= 3:
+        print("You lose the game...\n")
+    else:
+        playerChooses()
+
+rpsChoices = ('Rock', 'Paper', 'Scissors')
+playerScore = 0
+compScore = 0
+
+print("\nWelcome to Rock, Paper, Scissors!\n")
+sleep(1)
+print("Do you want to play?")
+
+playerStartCheck()
+
+
---
+++ RPS_ProjectSummary.md
@@ -0,0 +1,10 @@
+# Project: Rock, Paper, Scissors Game
+
+ - v1.0 Completed: 5/10/24
+
+ - Concepts used: Module importing, defining functions,
+recursive functions, user input, setting global variables,
+branching logic
+
+This is a simple Python project I wrote to practice some core
+concepts, as well as acquaint myself with Python-specific syntax.
